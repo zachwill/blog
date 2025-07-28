de459ed27e544e12fc4f9f369fec7eea1928fd31
Zach Williams <hey@zachwill.com>
2025-07-28 13:44:52 -0700

    I'm going to default to wa-light

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -63,9 +63,9 @@ export default function AppShell({
       </head>
 
       <body
-        className="wa-palette-zach wa-theme-zach wa-cloak"
+        className="wa-palette-zach wa-theme-zach wa-cloak wa-light"
         data-signals-webawesome="!document.querySelector('body').classList.contains('wa-cloak')"
-        data-class-wa-dak="window.matchMedia('(prefers-color-scheme: dark)').matches"
+        { /* data-class-wa-dark="window.matchMedia('(prefers-color-scheme: dark)').matches" */}
         data-effect="if ($webawesome) {
           const nav = document.querySelector('nav .current');
           const article = document.querySelector('main h1');
