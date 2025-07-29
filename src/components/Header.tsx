7263645e2bdf27bf4386167d6860c6f2d04d50bb
Zach Williams <hey@zachwill.com>
2025-07-29 13:58:19 -0700

    Move tooltip before button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,14 +17,14 @@ export function Header() {
       </div>
 
       <div className="wa-cluster wa-gap-xs">
+        <wa-tooltip for="zach-twitter">Twitter</wa-tooltip>
         <wa-button id="zach-twitter" appearance="plain" size="small" aria-label="Twitter" href={siteConfig.social.twitter} target="_blank">
           <wa-icon label="Twitter" name="at"></wa-icon>
         </wa-button>
-        <wa-tooltip for="zach-twitter">Twitter</wa-tooltip>
+        <wa-tooltip for="zach-github">GitHub</wa-tooltip>
         <wa-button id="zach-github" appearance="plain" size="small" aria-label="GitHub" href={siteConfig.social.github} target="_blank">
           <wa-icon label="GitHub" name="code"></wa-icon>
         </wa-button>
-        <wa-tooltip for="zach-github">GitHub</wa-tooltip>
       </div>
     </header>
   );
