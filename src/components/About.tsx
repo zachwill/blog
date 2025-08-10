913a798d7a4099aecf1e880367b655f2b8d66273
Zach Williams <hey@zachwill.com>
2025-08-09 21:57:56 -0700

    Add Datastar link to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,10 +15,15 @@ export function About({ heading = 'xl', fontSize = 'l', gap = 'm' }) {
         <wa-tooltip for="zach-nba">NBA.com</wa-tooltip>
         I'm currently <a id="zach-linkedin" href="https://linkedin.com/in/heyzachwill">Director of Data Science</a> for the <a id="zach-nba" href="https://www.nba.com/blazers/zach-williams">Portland Trail Blazers</a>.
       </div>
-      <div style={{ fontSize: `var(--wa-font-size-${fontSize})` }}>
-        <wa-tooltip for="zach-github-repos">GitHub</wa-tooltip>
-        Several of my open-source projects are on <a id="zach-github-repos" href="https://github.com/zachwill">GitHub</a>.
-      </div>
+
+      <a href="/datastar">
+        <wa-callout variant="brand">
+          <wa-icon slot="icon" name="rocket"></wa-icon>
+          <div className="wa-align-items-baseline">
+            <span style={{ fontFamily: 'var(--wa-font-family-longform)', fontWeight: 'var(--wa-font-weight-normal)', fontStyle: 'italic' }}>Author's Note:&nbsp;</span> Check out my recent deep dive into Datastar here &nbsp;<wa-icon name="arrow-up-right-from-square" style={{ fontSize: 'var(--wa-font-size-xs)' }}></wa-icon>
+          </div>
+        </wa-callout>
+      </a>
     </div >
   );
 }
